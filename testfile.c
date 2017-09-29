8f5e167a7086949ad60e72db2a130dc84c917523
JoeGorm <joegormley@live.com>
2017-09-29 18:17:58 -0400

    No summary.
    This is a test description.

--- testfile.c
+++ testfile.c
@@ -5,5 +5,6 @@
 int main(int argc, int** argv) {
 	printf("This is a test, committed by Bruno 5:57pm\n");
 	printf("This is not a test, this is a very very serious commit by Alex\n");
+    printf("Alex, it's a robot.");
 	return 0;
 }
